a95ab081cab476a802f0ce46e44d5ca0265a6a27
Joao victor sg <jvictorsg014@gmail.com>
2023-11-21 21:56:56 -0300

    Validação de cpf

--- 04-formulario/script/valida-cpf.js
+++ 04-formulario/script/valida-cpf.js
@@ -14,30 +14,51 @@ document.getElementById("validarButton").
 function validaCPF(vrCPF) {
     //Implementar as regras de validação do CPF
     let soma = 0;
-    soma+= parseInt ( vrCPF.charAt(0 ) ) * 10;
-    soma+= parseInt ( vrCPF.charAt(1 ) ) * 9;
-    soma+= parseInt ( vrCPF.charAt(2 ) ) * 8;
-    soma+= parseInt ( vrCPF.charAt(3 ) ) * 7;
-    soma+= parseInt ( vrCPF.charAt(4 ) ) * 6;
-    soma+= parseInt ( vrCPF.charAt(5 ) ) * 5;
-    soma+= parseInt ( vrCPF.charAt(6 ) ) * 4;
-    soma+= parseInt ( vrCPF.charAt(7 ) ) * 3;
-    soma+= parseInt ( vrCPF.charAt(8 ) ) * 2;
+    soma += parseInt(vrCPF.charAt(0)) * 10;
+    soma += parseInt(vrCPF.charAt(1)) * 9;
+    soma += parseInt(vrCPF.charAt(2)) * 8;
+    soma += parseInt(vrCPF.charAt(3)) * 7;
+    soma += parseInt(vrCPF.charAt(4)) * 6;
+    soma += parseInt(vrCPF.charAt(5)) * 5;
+    soma += parseInt(vrCPF.charAt(6)) * 4;
+    soma += parseInt(vrCPF.charAt(7)) * 3;
+    soma += parseInt(vrCPF.charAt(8)) * 2;
 
     let digitoX = soma%11;
     digitoX = 11 - digitoX;
-
-    if(digitoX > 10 ) {
+    if (digitoX >= 10) {
         digitoX = 0
     }
 
-    let digitValido = true;
+    soma = 0 ; 
+    soma += parseInt(vrCPF.charAt(0)) * 11;
+    soma += parseInt(vrCPF.charAt(1)) * 10;
+    soma += parseInt(vrCPF.charAt(2)) * 9;
+    soma += parseInt(vrCPF.charAt(3)) * 8;
+    soma += parseInt(vrCPF.charAt(4)) * 7;
+    soma += parseInt(vrCPF.charAt(5)) * 6;
+    soma += parseInt(vrCPF.charAt(6)) * 5;
+    soma += parseInt(vrCPF.charAt(7)) * 4;
+    soma += parseInt(vrCPF.charAt(8)) * 3;
+    soma += parseInt(vrCPF.charAt(9)) * 2;
 
-    if(digitoX !== parseInt (vrCPF.charAt(9) ) ) {
+    let digitoY = soma%11;
+    digitoY = 11 - digitoY;
+    if (digitoY >= 10) {
+        digitoY = 0
+    }
+
+    let digitValido = true;
+    if (digitoX !== parseInt(vrCPF.charAt(9))) {
+        digitValido = false;
+    }
+    if (digitoY !== parseInt(vrCPF.charAt(10))) {
         digitValido = false;
     }
 
-    return digitValido;   
-    //return vrCPF.length === 11 & !isNaN(vrCPF)    
+        return digitValido;
+    //return vrCPF.length === 11 & !isNaN(vrCPF) 
+
+       
 }
 
